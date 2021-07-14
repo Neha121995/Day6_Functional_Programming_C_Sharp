af53e7945f3e2f8a4cefd50c3d15221072b2e9e0
Neha121995 <jagtapneha1212@gmail.com>
2021-07-14 23:28:14 +0530

    [Neha] added: Prime Factors of number N

---
+++ PrimeFactorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrimeFactors
+{
+    class PrimeFactorial
+    {
+        public static void primeFactors(int n)
+        {
+            while (n%2 == 0)
+            {
+                Console.WriteLine(2 + " ");
+                n /= 2;
+            }
+            for (int i=3; i<=Math.Sqrt(n); i+=2)
+            {
+                while (n%i==0)
+                {
+                    Console.WriteLine(i + " ");
+                    n /= i;
+                }
+            }
+            if (n>2)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = 315;
+            primeFactors(n);
+        }
+    }
+}
