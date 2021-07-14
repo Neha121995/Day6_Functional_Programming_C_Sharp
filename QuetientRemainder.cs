698ef9488a66ffb5d67e8454031aca6831c1d9f0
Neha121995 <jagtapneha1212@gmail.com>
2021-07-14 23:29:14 +0530

    [Neha] added: Quotient and Remainder Program

---
+++ QuetientRemainder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace QuetientAndReminder
+{
+    class QuetientRemainder
+    {
+        static void Main(string[] args)
+        {
+            int dividend = 50, divisor = 8;
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Divident:{0} Divisor:{1}" , dividend,divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
