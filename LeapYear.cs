4947e66e1fc37d1f2b125da1c585816e800e8de6
Neha121995 <jagtapneha1212@gmail.com>
2021-07-14 18:59:42 +0530

    [Neha] added: Check Leap Year or not

---
+++ LeapYear.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LeapYearProblem
+{
+    class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            //int year = 2020;
+            Console.WriteLine("Enter Year (YYYY) :: ");
+            int year = int.Parse(Console.ReadLine());
+            if ((year % 4) == 0)
+            {
+                if ((year % 100) == 0)
+                {
+                    if ((year % 400) == 0)
+                    {
+                        Console.WriteLine("This is a leap year");
+                    }
+                    else
+                    {
+                        Console.WriteLine("This is not leap year");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("This is a leap year");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("This is not leap year");
+            }
+            
+        }
+
+    }
+}
