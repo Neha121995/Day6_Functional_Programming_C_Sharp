160ca0ea02eac3cabd81e5c7358265c2245141fa
Neha121995 <jagtapneha1212@gmail.com>
2021-07-14 22:30:27 +0530

    [Neha] added: Power of two

---
+++ PowerOfTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PowerOfTwo
+{
+    class PowerOfTwo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number :: ");
+            int n = int.Parse(Console.ReadLine());
+            int i;
+            int power = 0;
+            for (i = 0; i<=n; i++)
+            {
+                if(i == 0)
+                {
+                    power = 1;
+                }
+                else
+                {
+                    power = power * 2;
+                }
+                
+            }
+            Console.WriteLine(power);
+
+        }
+    }
+}
