bfda1434dbe9dce0d12b467184bf6ce3c71c4d42
Neha121995 <jagtapneha1212@gmail.com>
2021-07-14 18:56:19 +0530

    [Neha] added: Flip Coin Program

---
+++ FlipCoin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    class FlipCoin
+    {
+        static void Main(string[] args)
+        {
+            int noOfTime;
+            int noOfHeads = 0;
+            int noOfTails = 0;
+            int noOfItration = 0;
+
+            Console.WriteLine("How many times you want to flip the coin :");
+            noOfTime = int.Parse(Console.ReadLine());
+
+            
+            while (noOfItration <= noOfTime)
+            {
+                noOfItration++;
+                Random random = new Random();
+                double coinFlip = random.NextDouble();
+                //Console.WriteLine("Check"+coinFlip);
+
+
+                if (coinFlip < 0.5)
+                {
+                    Console.WriteLine("You got Tails.");
+                    noOfTails++;
+                }
+                else
+                {
+                    Console.WriteLine("You got Heads.");
+                    noOfHeads++;
+                }
+
+            }
+            double pHead = noOfHeads * 100 / noOfTime;
+            Console.WriteLine("Heads is " + pHead + " Percent ");
+            double pTail = noOfTails * 100 / noOfTime;
+            Console.WriteLine("Tails is " + pTail + " Percent ");
+
+        }
+    }
+}
