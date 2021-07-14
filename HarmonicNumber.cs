8e8a4b63db02fa532fbbdf7af45ebcd36baac613
Neha121995 <jagtapneha1212@gmail.com>
2021-07-14 22:31:54 +0530

    [Neha] added: Harmonic Number

---
+++ HarmonicNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    class HarmonicNumber
+    {
+        static void Main(string[] args)
+        {
+            
+            float i;
+            float result = 0;
+            Console.WriteLine("Enter the number : ");
+            float n = int.Parse(Console.ReadLine());
+            float div = 0;
+
+            for (i=1; i<=n; i++)
+            {
+                
+                div = 1 / i;
+                result = result + div;
+            }
+            Console.WriteLine(result + " is HarmonicNumber number");
+        }
+    }
+}
